5ff6bac96a16e681814314465055df95248879dc
Damini Varu <damvaru4@gmail.com>
2019-06-16 22:16:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string) {
-  return string.mixedCase.toLowerCase()
-  return string.mixedCase.toUpperCase()
-}
