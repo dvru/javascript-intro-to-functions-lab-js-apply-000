924c0069b41ee208681b889d621531487146c1e1
Damini Varu <damvaru4@gmail.com>
2019-06-16 22:52:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+} 
 function logShout(string) {
-   console.log(string.toUpperCase()
+  console.log( string.toUpperCase())
 }
-
-
-
-
-
+function logWhisper(string) {
+  console.log( string.toLowerCase())
+}
 
 
 function sayHiToGrandma(string){
